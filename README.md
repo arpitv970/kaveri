f280cd44b990ed5b0d878f194b086d992e96fd8b
Arpit Verma <arpitv970@gmail.com>
2023-09-10 20:21:06 +0530

    asdf

---
+++ README.md
@@ -0,0 +1,46 @@
+# AI Powered Chat Application
+
+> ⚠️This application is currently under-devlopment, and it is Open for your precious contribution
+
+---
+### Join conversation with team 👇
+[![Discord](https://img.shields.io/discord/807214956357484583?color=blue&label=Discord%20Server&logo=Cold%20Spin%20Server&style=for-the-badge)](https://discord.gg/xR6qCrU8mu)
+
+---
+
+This application is dedicated to people who value time, so that they could save time by **not writing** the similar prompt again and again. Simply login & start saving your prompts. Also, discover prompts by other people involved in the community.
+
+# Getting Started
+## Local Setup
+Follow the procedure mentioned just below, to setup this application in your local machine:
+- Fork this repository to your GitHub Account
+- Now simply clone it to your machine
+- After cloning the application, jump right into the `prompt-gen` directory
+  ```bash
+  cd prompt-gen
+  ```
+
+## Initiating project on local machine
+- Inside `prompt-gen` directory, generate the `node_modules` for the dependencies.
+    ```bash
+    npm install
+    ```
+- After installing dependencies, run the project locally
+  - To run dev scripts
+    ```bash
+    npm run dev
+    ```
+- To run at deployment server
+    ```bash
+    npm start
+    ```
+
+### Now you have done with installation & running the project to your local machine, feel free to contribute to the project 🤗
+
+## Task TO-DO
+- Search
+    - By prompt
+    - By tags
+    - By User name
+- visit other profile
+- add error pages
---
+++ components/Feed.jsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import PromptCard from './PromptCard';
+import { useSession } from 'next-auth/react';
+
+const PromptCardList = ({ data, handleTagClick }) => {
+    return (
+        <div className='mt-10 prompt_layout'>
+            {data &&
+                data.map((post) => (
+                    <PromptCard
+                        key={post._id}
+                        post={post}
+                        handleTagClick={handleTagClick}
+                    />
+                ))}
+        </div>
+    );
+};
+
+const Feed = () => {
+    const [searchText, setSearchText] = useState('');
+    const [posts, setPosts] = useState([]);
+    const { data: session } = useSession();
+
+    useEffect(() => {
+        const fetchPost = async () => {
+            const res = await fetch('/api/prompt');
+            const data = await res.json();
+
+            setPosts(data);
+        };
+
+        if (session?.user.id) {
+            fetchPost();
+        }
+    }, [session]);
+
+    const handleSearchChange = (e) => {
+        e.preventDefault();
+        setSearchText(e.target.value);
+        console.log(e.target.value);
+    };
+    return (
+        <section className='feed'>
+            <form
+                onChange={handleSearchChange}
+                className='relative w-full flex-center'
+            >
+                <input
+                    type='text'
+                    placeholder='Search for a tag or a username'
+                    value={searchText}
+                    required
+                    className='search_input peer'
+                />
+            </form>
+
+            <PromptCardList data={posts} handleTagClick={() => {}} />
+        </section>
+    );
+};
+
+export default Feed;
